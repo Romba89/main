a2cdee26d1d821877e47b469fc0c173194732287
she'll be <88987121+Romba89@users.noreply.github.com>
2023-02-12 03:24:27 +0100

    Add files via upload

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -35,12 +35,12 @@ ver=$VERSION_ID
 
 #detail nama perusahaan
 country=ID
-state=Indonesia
-locality=Indonesia
-organization=akbarstorevpn
-organizationalunit=akbarstorevpn
-commonname=akbarstorevpn
-email=akbarssh21@gmail.com
+state=Germany
+locality=Germany
+organization=mahboubvpn
+organizationalunit=mahboubvpn
+commonname=mahboubvpn
+email=Tunnelcatvpn@gmail.com
 
 # simple password minimal
 wget -O /etc/pam.d/common-password "https://${akbarvpn}/password"
@@ -390,7 +390,7 @@ netfilter-persistent reload
 cd /usr/bin
 wget -O addhost "https://${akbarvpn}/addhost.sh"
 wget -O about "https://${akbarvpn}/about.sh"
-wget -O menu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/menu.sh"
+wget -O menu "https://raw.githubusercontent.com/Romba89/main/main/update/menu.sh"
 wget -O addssh "https://${akbarvpn}/addssh.sh"
 wget -O trialssh "https://${akbarvpn}/trialssh.sh"
 wget -O delssh "https://${akbarvpn}/delssh.sh"
@@ -433,21 +433,21 @@ wget -O addtrgo "https://${akbarvpnnn}/addtrgo.sh"
 wget -O deltrgo "https://${akbarvpnnn}/deltrgo.sh"
 wget -O renewtrgo "https://${akbarvpnnn}/renewtrgo.sh"
 wget -O cektrgo "https://${akbarvpnnn}/cektrgo.sh"
-wget -O portsshnontls "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/websocket/portsshnontls.sh"
-wget -O portsshws "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/websocket/portsshws.sh"
+wget -O portsshnontls "https://raw.githubusercontent.com/Romba89/main/main/websocket/ws-nontls.sh"
+wget -O portsshws "https://raw.githubusercontent.com/Romba89/main/main/websocket/portsshws.sh"
 
-wget -O sshovpnmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/sshovpn.sh"
-wget -O l2tpmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/l2tpmenu.sh"
-wget -O pptpmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/pptpmenu.sh"
-wget -O sstpmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/sstpmenu.sh"
-wget -O wgmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/wgmenu.sh"
-wget -O ssmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/ssmenu.sh"
-wget -O ssrmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/ssrmenu.sh"
-wget -O vmessmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/vmessmenu.sh"
-wget -O vlessmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/vlessmenu.sh"
-wget -O trmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/trmenu.sh"
-wget -O trgomenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/trgomenu.sh"
-wget -O setmenu "https://raw.githubusercontent.com/senowahyu62/scriptvps/main/update/setmenu.sh"
+wget -O sshovpnmenu "https://raw.githubusercontent.com/Romba89/main/main/update/sshovpn.sh"
+wget -O l2tpmenu "https://raw.githubusercontent.com/Romba89/main/main/update/l2tpmenu.sh"
+wget -O pptpmenu "https://raw.githubusercontent.com/Romba89/main/main/update/pptpmenu.sh"
+wget -O sstpmenu "https://raw.githubusercontent.com/Romba89/main/main/update/sstpmenu.sh"
+wget -O wgmenu "https://raw.githubusercontent.com/Romba89/main/main/update/wgmenu.sh"
+wget -O ssmenu "https://raw.githubusercontent.com/Romba89/main/main/update/ssmenu.sh"
+wget -O ssrmenu "https://raw.githubusercontent.com/Romba89/main/main/update/ssrmenu.sh"
+wget -O vmessmenu "https://raw.githubusercontent.com/Romba89/main/main/update/vmessmenu.sh"
+wget -O vlessmenu "https://raw.githubusercontent.com/Romba89/main/main/update/vlessmenu.sh"
+wget -O trmenu "https://raw.githubusercontent.com/Romba89/main/main/update/trmenu.sh"
+wget -O trgomenu "https://raw.githubusercontent.com/Romba89/main/main/update/trgomenu.sh"
+wget -O setmenu "https://raw.githubusercontent.com/Romba89/main/main/update/setmenu.sh"
 
 
 chmod +x portsshnontls
